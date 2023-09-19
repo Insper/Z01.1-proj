7ba180f3115d7f3f282dcdc1f48e0662d0c2ecfd
Licia Sales <licia.sales@hotmail.com>
2023-09-19 06:22:37 -0300

    retirando o add16 e mux16 do teste

--- c_ULA/test_ula.py
+++ c_ULA/test_ula.py
@@ -48,16 +48,6 @@ def test_inc16():
     run(vhdl_sources=[source("inc16.vhd"), source('add16.vhd'), source('fulladder.vhd')], toplevel="inc16", module="ula_cocotb" , testcase='tb_inc16', toplevel_lang="vhdl")
 
 
-@pytest.mark.telemetry_files(source('and16.vhd'))
-def test_and16():
-    run(vhdl_sources=[source("and16.vhd")], toplevel="and16", module="ula_cocotb" , testcase='tb_and16', toplevel_lang="vhdl")
-
-
-@pytest.mark.telemetry_files(source('mux16.vhd'))
-def test_mux16():
-    run(vhdl_sources=[source("mux16.vhd")], toplevel="mux16", module="ula_cocotb" , testcase='tb_mux16', toplevel_lang="vhdl")
-
-
 @pytest.mark.telemetry_files(source("alu.vhd"),source("zerador16.vhd"), source("inversor16.vhd"), source("add16.vhd"), source("fulladder.vhd"), source("../../b_logComb/src/and16.vhd"), source("comparador16.vhd"), source("../../b_logComb/src/mux16.vhd"))
 def test_alu():
     run(vhdl_sources=[source("alu.vhd"),source("zerador16.vhd"), source("inversor16.vhd"), source("add16.vhd"), source("fulladder.vhd"), source("../../b_logComb/src/and16.vhd"), source("comparador16.vhd"), source("../../b_logComb/src/mux16.vhd")], toplevel="alu", module="ula_cocotb" , testcase='tb_alu', toplevel_lang="vhdl")
